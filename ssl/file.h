5c89ff3c1826c4976957253ded1c5bdd86310162
FreezB11 <yashrajrao4@gmail.com>
2024-09-06 18:01:38 +0530

    minor update

--- ssl/file.h
+++
@@ -1,6 +0,0 @@
-#ifndef FILE_H
-#define FILE_H
-
-char *load_file_into_memory( char *filename, int *buffer_length );
-
-#endif
--- ssl/hex.h
+++
@@ -1,7 +0,0 @@
-#ifndef HEX_H
-#define HEX_H
-
-int hex_decode( const unsigned char *input, unsigned char **decoded );
-void show_hex( const unsigned char *array, int length );
-
-#endif
