b8549d98e1c4fadf7ee8917a04e2b4114008d006
FreezB11 <yashrajrao4@gmail.com>
2024-09-24 01:38:59 +0530

    learning the base64

--- tls-0.1/base64.h
+++ tls-0.1/base64.h
@@ -1,6 +1,16 @@
 #ifndef BASE64_H
 #define BASE64_H
 
+/*
+
+    the base64 divides the input into 
+    6-bit chunks { 2^6 = 64 }
+    64 -> 52 (a-z) + (A-Z)
+       -> 10 (0-9)
+       -> 2(left to map) arbitarily (+,/)
+
+*/
+
 void base64_encode( const unsigned char *input, int len, unsigned char *output );
 int base64_decode( const unsigned char *input, int len, unsigned char *output );
 
